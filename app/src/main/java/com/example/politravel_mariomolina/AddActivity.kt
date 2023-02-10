9d11cfe9381ba6b193be73f717fd891e38d87579
MarioMolina27 <molinaballesteros@hotmail.com>
2023-02-10 10:11:10 +0100

    xml dialog

--- app/src/main/java/com/example/politravel_mariomolina/AddActivity.kt
+++ app/src/main/java/com/example/politravel_mariomolina/AddActivity.kt
@@ -2,6 +2,11 @@ package com.example.politravel_mariomolina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.ListView
 import android.widget.Spinner
 
 class AddActivity : AppCompatActivity() {
@@ -18,9 +23,22 @@ class AddActivity : AppCompatActivity() {
         val lstTransport = findViewById<Spinner>(R.id.lstTransport)
         val adapter = TransportAdapter(this,R.layout.transport_item,transport)
         lstTransport.adapter=adapter
-
         adapter.setDropDownViewResource(R.layout.transport_item)
 
+        val edtNomPaquet = findViewById<EditText>(R.id.edtxtNomPaquet)
+        val edtDuracio = findViewById<EditText>(R.id.edtxtDuracio)
+        val edtDescripcio = findViewById<EditText>(R.id.edtxtDescripcio)
+        val edtZoom = findViewById<EditText>(R.id.edtxtZoomMaps)
+        val lstItinerariAdd = findViewById<ListView>(R.id.lstItinerariAdd)
+        val btnAddItinerari= findViewById<Button>(R.id.btnAddItinerari)
+        val btnAcceptarNouPaquet = findViewById<Button>(R.id.btnAcceptarNouPaquet)
+        val btnAddPrincipalImg = findViewById<ImageButton>(R.id.btnAddPrincipalImg)
+        val btnAddSecundariaImg = findViewById<ImageButton>(R.id.btnAddSecundariaImg)
+
+        btnAddItinerari.setOnClickListener()
+        {
+
+        }
 
     }
 }
